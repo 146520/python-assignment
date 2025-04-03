63d13e4fcd3749eb7d36390ab2bbaaebc59884e8
nelima <mercy.w.nelima@gmail.com>
2025-04-03 19:35:30 +0300

    assignment 2

---
+++ python_assignment2.py
@@ -0,0 +1,27 @@
+# Step 1
+my_list = []
+
+# Step 2
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+# Step 3
+my_list.insert(1, 15)
+
+# Step 4
+my_list.extend([50, 60, 70])
+
+# Step  5
+my_list.pop()
+
+# Step 6
+my_list.sort()
+
+# Step 7
+print(my_list.index(30))
+
+
+
+
